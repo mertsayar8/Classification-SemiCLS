b42c71964b7a2740ddcc26e25528abb06ddbb4ce
Mert Sayar <mert.sayar@gmail.com>
2022-06-16 13:49:12 +0200

    Update builder.py

--- models/builder.py
+++ models/builder.py
@@ -3,6 +3,7 @@ import logging
 from copy import deepcopy
 
 from models import  resnet, wideresnet
+from models.resnet-mlmi import get_resnet18
 
 models = {
     "wideresnet": wideresnet.build,
@@ -17,6 +18,19 @@ def build(cfg, logger=None):
     # init params
     init_params = deepcopy(cfg)
     type_name = init_params.pop("type")
+    
+    if type_name == "resnet18mlmi":
+        model = get_resnet18()
+        if logger is not None:
+            logger.info("{} Total params: {:.2f}M".format(
+                type_name, sum(p.numel() for p in model.parameters()) / 1e6))
+        else:
+            logging.info("{} Total params: {:.2f}M".format(
+                type_name, sum(p.numel() for p in model.parameters()) / 1e6))
+        return model
+        
+    
+    
 
     # init model
     model = models[type_name](**init_params)
